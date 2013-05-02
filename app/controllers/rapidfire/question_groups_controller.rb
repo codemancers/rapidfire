b88e631902125c052b7b35551717b94672e1bd63
Yuva <yuva@codemancers.com>
2013-05-03 02:02:42 +0530

    add support for creating question groups

--- app/controllers/rapidfire/question_groups_controller.rb
+++ app/controllers/rapidfire/question_groups_controller.rb
@@ -1,4 +1,23 @@
 module Rapidfire
   class QuestionGroupsController < ApplicationController
+    respond_to :html
+
+    def index
+      @question_groups = QuestionGroup.all
+      respond_with(@question_groups)
+    end
+
+    def show
+      @question_group = QuestionGroup.find(params[:id])
+      respond_with(@question_group)
+    end
+
+    def create
+      @question_group = QuestionGroup.new
+      @question_group.save
+
+      location = question_group_questions_path(@question_group)
+      respond_with(@question_group, :location => location)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rapidfire::Engine.routes.draw do
+  resources :question_groups do
+    resources :questions
+  end
+
+  resources :answer_groups do
+    resources :answers
+  end
+
+  root :to => "question_groups#index"
 end
