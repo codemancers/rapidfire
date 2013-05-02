087d190f2263543b9bc78c809d615090fece629a
Yuva <yuva@codemancers.com>
2013-05-03 01:41:18 +0530

    remove created generators and empty rapidfire tasks

--- lib/generators/rapidfire/templates/create_answer_groups.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnswerGroups < ActiveRecord::Migration
-  def change
-    create_table(:answer_groups) do |t|
-      t.references  :question_group
-
-      t.timestamps
-    end
-  end
-end
--- lib/generators/rapidfire/templates/create_question_groups.rb
+++
@@ -1,7 +0,0 @@
-class CreateQuestionGroups < ActiveRecord::Migration
-  def change
-    create_table(:question_groups) do |t|
-      t.timestamps
-    end
-  end
-end
