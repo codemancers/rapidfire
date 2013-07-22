45c1db8fe5b95eaaecafbb2b0265a336de15b712
Yuva <yuva@codemancers.com>
2013-07-23 02:12:04 +0530

    wip: add crude logic for extracting results

--- app/models/rapidfire/question_group_results.rb
+++ app/models/rapidfire/question_group_results.rb
@@ -1,5 +1,13 @@
 module Rapidfire
   class QuestionGroupResults
+    class Result
+      def initialize(question, results)
+        @question, @results = question, results
+      end
+      attr_accessor :question, :results
+    end
+
+
     def initialize(question_group)
       @question_group = question_group
     end
@@ -9,9 +17,23 @@ module Rapidfire
     # each entry will have the following:
     # 1. question type and question id
     # 2. question text
-    # 3. if question type is aggregetable, each option with value
-    # 4. else an array of all the answers
+    # 3. if aggregatable, return each option with value
+    # 4. else return an array of all the answers given
     def extract
+      @question_group.questions.collect do |question|
+        results =
+          case question.class
+          when Rapidfire::Questions::Select, Rapidfire::Questions::Radio,
+            Rapidfire::Questions::Checkbox
+            question.answers.group(:answer_text).count
+
+          when Rapidfire::Questions::Short, Rapidfire::Questions::Date,
+            Rapidfire::Questions::Long, Rapidfire::Questions::Numeric
+            question.answers.pluck(:answer_text)
+          end
+
+        Result.new(question, results)
+      end
     end
   end
 end
