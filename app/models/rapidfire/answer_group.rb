2e9e42af71601729ef093b6e1d46edb6f19a548f
Regis A. Despres <regis.despres@gmail.com>
2013-09-30 02:25:28 +0200

    Update answer_group.rb
    Rails 4 need it too =)

--- app/models/rapidfire/answer_group.rb
+++ app/models/rapidfire/answer_group.rb
@@ -4,7 +4,7 @@ module Rapidfire
     belongs_to :user, polymorphic: true
     has_many   :answers, inverse_of: :answer_group, autosave: true
 
-    if Rails::VERSION::MAJOR == 3
+    if Rails::VERSION::MAJOR >= 3
       attr_accessible :question_group, :user
     end
   end
