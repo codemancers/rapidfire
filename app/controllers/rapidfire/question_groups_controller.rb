c345ea102ec6756fce7b80a4b5aa6639505c8e46
Yuva <yuva@codemancers.com>
2015-01-31 17:51:06 +0530

    as rails 4.2 has removed responders, remove respond_to and respond_with

--- app/controllers/rapidfire/question_groups_controller.rb
+++ app/controllers/rapidfire/question_groups_controller.rb
@@ -1,31 +1,23 @@
 module Rapidfire
   class QuestionGroupsController < Rapidfire::ApplicationController
     before_filter :authenticate_administrator!, except: :index
-    respond_to :html, :js
-    respond_to :json, only: :results
 
     def index
       @question_groups = QuestionGroup.all
-      respond_with(@question_groups)
     end
 
     def new
       @question_group = QuestionGroup.new
-      respond_with(@question_group)
     end
 
     def create
       @question_group = QuestionGroup.new(question_group_params)
       @question_group.save
-
-      respond_with(@question_group, location: rapidfire.question_groups_url)
     end
 
     def destroy
       @question_group = QuestionGroup.find(params[:id])
       @question_group.destroy
-
-      respond_with(@question_group)
     end
 
     def results
@@ -33,7 +25,9 @@ module Rapidfire
       @question_group_results =
         QuestionGroupResults.new(question_group: @question_group).extract
 
-      respond_with(@question_group_results, root: false)
+      respond_to do |format|
+        format.json { @question_group_results.to_json }
+      end
     end
 
     private
--- app/controllers/rapidfire/questions_controller.rb
+++ app/controllers/rapidfire/questions_controller.rb
@@ -1,48 +1,40 @@
 module Rapidfire
   class QuestionsController < Rapidfire::ApplicationController
     before_filter :authenticate_administrator!
-    respond_to :html, :js
 
     before_filter :find_question_group!
     before_filter :find_question!, :only => [:edit, :update, :destroy]
 
     def index
       @questions = @question_group.questions
-      respond_with(@questions)
     end
 
     def new
       @question = QuestionForm.new(:question_group => @question_group)
-      respond_with(@question)
     end
 
     def create
       form_params = params[:question].merge(:question_group => @question_group)
       @question = QuestionForm.new(form_params)
       @question.save
-
-      respond_with(@question, location: index_location)
     end
 
     def edit
       @question = QuestionForm.new(:question => @question)
-      respond_with(@question)
     end
 
     def update
       form_params = params[:question].merge(:question => @question)
       @question = QuestionForm.new(form_params)
       @question.save
-
-      respond_with(@question, location: index_location)
     end
 
     def destroy
       @question.destroy
-      respond_with(@question, location: index_location)
     end
 
     private
+
     def find_question_group!
       @question_group = QuestionGroup.find(params[:question_group_id])
     end
