6ddb47ebec029b2a74a66cc2de7b343f8d2d349f
Bruno Casali <brunoocasali@gmail.com>
2015-09-12 00:58:34 -0300

    fixed D class file in codeclimate

--- app/controllers/rapidfire/questions_controller.rb
+++ app/controllers/rapidfire/questions_controller.rb
@@ -3,32 +3,20 @@ module Rapidfire
     before_filter :authenticate_administrator!
 
     before_filter :find_question_group!
-    before_filter :find_question!, :only => [:edit, :update, :destroy]
+    before_filter :find_question!, only: [:edit, :update, :destroy]
 
     def index
       @questions = @question_group.questions
     end
 
     def new
-      @question_form = QuestionForm.new(:question_group => @question_group)
+      @question_form = QuestionForm.new(question_group: @question_group)
     end
 
     def create
-      form_params = params[:question].merge(:question_group => @question_group)
-      @question_form = QuestionForm.new(form_params)
-      @question_form.save
+      form_params = params[:question].merge(question_group: @question_group)
 
-      if @question_form.errors.empty?
-        respond_to do |format|
-          format.html { redirect_to index_location }
-          format.js
-        end
-      else
-        respond_to do |format|
-          format.html { render :new }
-          format.js
-        end
-      end
+      save_and_redirect(params: form_params, error: :new)
     end
 
     def edit
@@ -36,21 +24,9 @@ module Rapidfire
     end
 
     def update
-      form_params = params[:question].merge(:question => @question)
-      @question_form = QuestionForm.new(form_params)
-      @question_form.save
+      form_params = params[:question].merge(question: @question)
 
-      if @question_form.errors.empty?
-        respond_to do |format|
-          format.html { redirect_to index_location }
-          format.js
-        end
-      else
-        respond_to do |format|
-          format.html { render :edit }
-          format.js
-        end
-      end
+      save_and_redirect(params: form_params, error: :edit)
     end
 
     def destroy
@@ -62,6 +38,23 @@ module Rapidfire
     end
 
     private
+
+    def save_and_redirect(params: required, error: required)
+      @question_form = QuestionForm.new(params)
+      @question_form.save
+
+      if @question_form.errors.empty?
+        respond_to do |format|
+          format.html { redirect_to index_location }
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html { render error }
+          format.js
+        end
+      end
+    end
 
     def find_question_group!
       @question_group = QuestionGroup.find(params[:question_group_id])
