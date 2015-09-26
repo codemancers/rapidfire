8d68bb4468ac9d45d8687ffbc05a4741cd8589c6
Yuva <yuva@codemancers.com>
2015-09-26 14:23:39 +0530

    explicitly specify class name in survey associations

--- app/models/rapidfire/attempt.rb
+++ app/models/rapidfire/attempt.rb
@@ -1,6 +1,6 @@
 module Rapidfire
   class Attempt < ActiveRecord::Base
-    belongs_to :survey
+    belongs_to :survey, class_name: 'Rapidfire::Survey'
     belongs_to :user, polymorphic: true
     has_many   :answers, inverse_of: :attempt, autosave: true
 
--- app/models/rapidfire/question.rb
+++ app/models/rapidfire/question.rb
@@ -1,6 +1,6 @@
 module Rapidfire
   class Question < ActiveRecord::Base
-    belongs_to :survey, :inverse_of => :questions
+    belongs_to :survey, :inverse_of => :questions, :class_name => 'Rapidfire::Survey'
     has_many   :answers
 
     default_scope { order(:position) }
