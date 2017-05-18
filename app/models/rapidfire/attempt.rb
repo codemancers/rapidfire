83b87b2a9d3d9bc134bf5c472b11f95ea55ae24d
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2017-05-18 00:00:22 -0700

    attr_accessible -> attr_accessor

--- app/models/rapidfire/attempt.rb
+++ app/models/rapidfire/attempt.rb
@@ -5,7 +5,7 @@ module Rapidfire
     has_many   :answers, inverse_of: :attempt, autosave: true
 
     if Rails::VERSION::MAJOR == 3
-      attr_accessible :survey, :user
+      attr_accessor :survey, :user
     end
   end
 end
