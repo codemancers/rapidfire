d74865706eff698d0c72b778a3574c3d161318bc
Sammy Larbi <sam@codeodor.com>
2017-07-08 14:09:34 -0500

    Fix to use specified logged in scope Rapidfire includes a method that looks to allow the programmer to specify `current_admin` or `current_student` or whatever scope of user they want, but it ends up using `current_user` anyway. This commit fixes that issue, and renames the method to something that is obviously in use for rapidfire, as opposed to the general `scoped` name.

--- app/controllers/rapidfire/attempts_controller.rb
+++ app/controllers/rapidfire/attempts_controller.rb
@@ -38,7 +38,7 @@ module Rapidfire
 
     def attempt_params
       answer_params = { params: (params[:attempt] || {}) }
-      answer_params.merge(user: current_user, survey: @survey, attempt_id: params[:id])
+      answer_params.merge(user: rapidfire_current_scoped, survey: @survey, attempt_id: params[:id])
     end
 
     # Override path to redirect after answer the survey
@@ -54,7 +54,7 @@ module Rapidfire
     end
 
     def rapidfire_current_scoped
-      send 'current_'+scoped.to_s
+      send 'current_' + rapidfire_scoped.to_s
     end
   end
 end
