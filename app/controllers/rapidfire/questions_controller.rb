b3d4764faf48314bdbe7b5e2fbbdb8b2db1b9355
Akshay <akshaysasidharan93@gmail.com>
2017-09-11 17:18:45 +0530

    before_filter method support

--- app/controllers/rapidfire/questions_controller.rb
+++ app/controllers/rapidfire/questions_controller.rb
@@ -1,9 +1,14 @@
 module Rapidfire
   class QuestionsController < Rapidfire::ApplicationController
-    before_action :authenticate_administrator!
-
-    before_action :find_survey!
-    before_action :find_question!, :only => [:edit, :update, :destroy]
+    if Rails::VERSION::MAJOR ==  5
+      before_action :authenticate_administrator!
+      before_action :find_survey!
+      before_action :find_question!, :only => [:edit, :update, :destroy]
+    else
+      before_filter :authenticate_administrator!
+      before_filter :find_survey!
+      before_filter :find_question!, :only => [:edit, :update, :destroy]
+    end
 
     def index
       @questions = @survey.questions
