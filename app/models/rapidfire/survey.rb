3d54c61435a4550e378514cd8a3d997ee5ef4dc9
Sammy Larbi <sam@codeodor.com>
2021-09-29 11:42:51 -0500

    Allow client code to define which attrs of user are included in CSV export

--- app/models/rapidfire/survey.rb
+++ app/models/rapidfire/survey.rb
@@ -12,17 +12,29 @@ module Rapidfire
       attr_accessible :name, :introduction, :after_survey_content
     end
 
+    def self.csv_user_attributes=(attributes)
+      @@csv_user_attributes = Array(attributes)
+    end
+
+    def self.csv_user_attributes
+      @@csv_user_attributes || []
+    end
+
     def results_to_csv(filter)
       CSV.generate do |csv|
         header = []
-        header << "User"
+        header += Rapidfire::Survey.csv_user_attributes
         questions.each do |question|
           header << question.question_text
         end
         csv << header
         attempts.where(SurveyResults.filter(filter, 'id')).each do |attempt|
           this_attempt = []
-          this_attempt << attempt.user.try(:survey_name)
+
+          Survey.csv_user_attributes.each do |attribute|
+            this_attempt << attempt.user.try(attribute)
+          end
+
           questions.each do |question|
             answer = attempt.answers.detect{|a| a.question_id == question.id }.try(:answer_text)
             this_attempt << answer
