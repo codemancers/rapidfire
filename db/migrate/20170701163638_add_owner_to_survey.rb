03a52127ef0af840f9723109ec1b2a0b0a9795f6
Yuva <yuva@codemancers.com>
2023-04-01 21:30:27 +0530

    Fix AR base issue in migrations

--- db/migrate/20170701163638_add_owner_to_survey.rb
+++ db/migrate/20170701163638_add_owner_to_survey.rb
@@ -1,4 +1,11 @@
-class AddOwnerToSurvey < ActiveRecord::Migration
+if Rails::VERSION::MAJOR >= 5
+  version = [Rails::VERSION::MAJOR, Rails::VERSION::MINOR].join('.').to_f
+  base = ActiveRecord::Migration[version]
+else
+  base = ActiveRecord::Migration
+end
+
+class AddOwnerToSurvey < base
   def change
     add_column :rapidfire_surveys, :owner_id, :integer
     add_column :rapidfire_surveys, :owner_type, :string
