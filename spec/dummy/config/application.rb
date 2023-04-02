1f38bf634afe8b9e68f6285772fadc3029ff0d10
Yuva <yuva@codemancers.com>
2023-04-02 13:17:08 +0530

    Load only required railties

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+if "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}" >= "5.2"
+  require "active_storage/engine"
+end
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
 
 Bundler.require(*Rails.groups)
 
