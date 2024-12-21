0e6f53389929aac2c052a385cd5d72a58ec1ea10
n1vedhar1 <nived.hari@codemancers.com>
2024-12-21 12:57:29 +0530

    remove version check

--- spec/models/rapidfire/questions/multi_file_spec.rb
+++ spec/models/rapidfire/questions/multi_file_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 def fixture_file_upload(path, mime_type)
   Rack::Test::UploadedFile.new(Pathname.new(file_fixture_path).join(path), mime_type, false)
@@ -7,52 +7,50 @@ end
 describe Rapidfire::Questions::MultiFile do
   describe "Validations" do
     it { is_expected.to validate_presence_of(:survey) }
-    it { is_expected.to validate_presence_of(:question_text)  }
+    it { is_expected.to validate_presence_of(:question_text) }
   end
 
   describe "Associations" do
     it { is_expected.to belong_to(:survey) }
   end
 
-  if "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}" >= "5.2"
-    describe "validate_answer" do
-      let(:question) { FactoryBot.create(:q_multifile, validation_rules: validation_rules) }
-      let(:answer) { FactoryBot.build(:answer, question: question, files: files) }
-      before  { answer.valid? }
+  describe "validate_answer" do
+    let(:question) { FactoryBot.create(:q_multifile, validation_rules: validation_rules) }
+    let(:answer) { FactoryBot.build(:answer, question: question, files: files) }
+    before { answer.valid? }
 
-      context "when there are no validation rules" do
-        let(:validation_rules) { {} }
+    context "when there are no validation rules" do
+      let(:validation_rules) { {} }
+      let(:files) { [] }
+
+      it "answer should pass validations" do
+        expect(answer.errors).to be_empty
+      end
+    end
+
+    context "when question needs file attachments" do
+      let(:validation_rules) { { presence: "1" } }
+
+      context "when answer is empty" do
         let(:files) { [] }
 
-        it "answer should pass validations" do
-          expect(answer.errors).to be_empty
+        it "fails validations" do
+          expect(answer.errors).not_to be_empty
+        end
+
+        it "says answer should be present" do
+          expect(answer.errors[:files]).to include("can't be blank")
         end
       end
 
-      context "when question needs a file attachment" do
-        let(:validation_rules) { { presence: "1" } }
-
-        context "when answer is empty" do
-          let(:files) { [] }
-
-          it "fails validations" do
-            expect(answer.errors).not_to be_empty
-          end
-
-          it "says answer should be present" do
-            expect(answer.errors[:files]).to include("can't be blank")
-          end
+      context "when answer is not empty" do
+        let(:files) do
+          [fixture_file_upload("one.txt", "text/plain"),
+           fixture_file_upload("two.txt", "text/plain")]
         end
 
-        context "when answer is not empty" do
-          let(:files) do
-            [fixture_file_upload("one.txt", "text/plain"),
-             fixture_file_upload("two.txt", "text/plain")]
-          end
-
-          it "passes validations" do
-            expect(answer.errors).to be_empty
-          end
+        it "passes validations" do
+          expect(answer.errors).to be_empty
         end
       end
     end
--- spec/services/rapidfire/attempt_builder_spec.rb
+++ spec/services/rapidfire/attempt_builder_spec.rb
@@ -1,45 +1,43 @@
-require 'spec_helper'
+require "spec_helper"
 
 def fixture_file_upload(path, mime_type)
   Rack::Test::UploadedFile.new(Pathname.new(file_fixture_path).join(path), mime_type, false)
 end
 
 describe Rapidfire::AttemptBuilder do
-  let(:survey)  { FactoryBot.create(:survey) }
-  let(:question1)  { FactoryBot.create(:q_short, survey: survey) }
-  let(:question2)  { FactoryBot.create(:q_long, survey: survey,
-                                        validation_rules: { presence: "1" }) }
+  let(:survey) { FactoryBot.create(:survey) }
+  let(:question1) { FactoryBot.create(:q_short, survey: survey) }
+  let(:question2) { FactoryBot.create(:q_long, survey: survey, validation_rules: { presence: "1" }) }
 
   describe "Creation" do
-    let(:builder)  { described_class.new(survey: survey) }
-    before  { [question1, question2] }
+    let(:builder) { described_class.new(survey: survey) }
+    before { [question1, question2] }
 
     it "builds answer group with answers" do
       expect(builder.answers).not_to be_empty
     end
 
-    it "builds answers based on number of questions available" do
+    it "builds answers based on the number of questions available" do
       questions = builder.answers.collect { |a| a.question }
       expect(questions).to match_array([question1, question2])
     end
   end
 
   describe "#save" do
-    let(:question_ids)  { survey.questions.map(&:id) }
+    let(:question_ids) { survey.questions.map(&:id) }
+    let(:answer_params) do
+      {
+        question1.id.to_s => { answer_text: "short answer" },
+        question2.id.to_s => { answer_text: "long answer" },
+      }
+    end
     let(:builder) do
-      params = { params: answer_params }.merge(survey: survey)
+      params = { params: answer_params, survey: survey }
       described_class.new(params)
     end
-    let(:save_answers)  { builder.save }
+    let(:save_answers) { builder.save }
 
     context "when all the answers are valid" do
-      let(:answer_params) do
-        {
-          question1.id.to_s => { :answer_text => "short answer" },
-          question2.id.to_s => { :answer_text => "long answer!" }
-        }
-      end
-
       it "returns true" do
         expect(save_answers).to be_truthy
       end
@@ -56,8 +54,8 @@ describe Rapidfire::AttemptBuilder do
     context "when some of the answers are invalid" do
       let(:answer_params) do
         {
-          question1.id.to_s => { :answer_text => "short answer" },
-          question2.id.to_s => { :answer_text => "" }
+          question1.id.to_s => { answer_text: "short answer" },
+          question2.id.to_s => { answer_text: "" },
         }
       end
 
@@ -69,67 +67,67 @@ describe Rapidfire::AttemptBuilder do
         save_answers
         expect(Rapidfire::Answer.count).to eq(0)
       end
+    end
 
-      if Rails::VERSION::MAJOR < 5
-        context "when requested to save without validations" do
-          let(:save_answers)  { builder.save(:validate => false) }
+    context "when requested to save without validations" do
+      let(:save_answers) { builder.save(validate: false) }
 
-          it "returns true" do
-            expect(save_answers).to be_truthy
-          end
+      it "returns true" do
+        expect(save_answers).to be_truthy
+      end
 
-          it "saves all the answers" do
-            save_answers
-            builder.answers.each do |answer|
-              expect(answer).to be_persisted
-              expect(question_ids).to include(answer.question_id)
-            end
-          end
+      it "saves all the answers" do
+        save_answers
+        builder.answers.each do |answer|
+          expect(answer).to be_persisted
+          expect(question_ids).to include(answer.question_id)
         end
       end
     end
 
-    if "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}" >= "5.2"
-      context "with a single file upload question" do
-        let(:question3) { FactoryBot.create(:q_file, survey: survey) }
+    context "with a single file upload question" do
+      let(:question3) { FactoryBot.create(:q_file, survey: survey) }
 
-        let(:answer_params) do
-          {
-            question1.id.to_s => { :answer_text => "short answer" },
-            question2.id.to_s => { :answer_text => "long answer" },
-            question3.id.to_s => { :file => fixture_file_upload("one.txt", "text/plain") }
-          }
-        end
-
-        it "saves the file successfully" do
-          expect(save_answers).to be_truthy
-
-          answer = Rapidfire::Answer.last
-          expect(answer).to be_persisted
-          expect(answer.file.download).to eq("one\n")
-        end
+      let(:answer_params) do
+        {
+          question1.id.to_s => { answer_text: "short answer" },
+          question2.id.to_s => { answer_text: "long answer" },
+          question3.id.to_s => { file: fixture_file_upload("one.txt", "text/plain") },
+        }
       end
 
-      context "with multiple files upload question" do
-        let(:question3) { FactoryBot.create(:q_multifile, survey: survey) }
+      it "saves the file successfully" do
+        expect(save_answers).to be_truthy
 
-        let(:answer_params) do
-          {
-            question1.id.to_s => { :answer_text => "short answer" },
-            question2.id.to_s => { :answer_text => "long answer" },
-            question3.id.to_s => { :files => [fixture_file_upload("one.txt", "text/plain"),
-                                              fixture_file_upload("two.txt", "text/plain")]}
-          }
-        end
+        answer = Rapidfire::Answer.last
+        expect(answer).to be_persisted
+        expect(answer.file.download).to eq("one\n")
+      end
+    end
 
-        it "saves the file successfully" do
-          expect(save_answers).to be_truthy
+    context "with multiple files upload question" do
+      let(:question3) { FactoryBot.create(:q_multifile, survey: survey) }
 
-          answer = Rapidfire::Answer.last
-          expect(answer).to be_persisted
-          expect(answer.files[0].download).to eq("one\n")
-          expect(answer.files[1].download).to eq("two\n")
-        end
+      let(:answer_params) do
+        {
+          question1.id.to_s => { answer_text: "short answer" },
+          question2.id.to_s => { answer_text: "long answer" },
+          question3.id.to_s => {
+            files: [
+              fixture_file_upload("one.txt", "text/plain"),
+              fixture_file_upload("two.txt", "text/plain"),
+            ],
+          },
+        }
+      end
+
+      it "saves the files successfully" do
+        expect(save_answers).to be_truthy
+
+        answer = Rapidfire::Answer.last
+        expect(answer).to be_persisted
+        expect(answer.files[0].download).to eq("one\n")
+        expect(answer.files[1].download).to eq("two\n")
       end
     end
   end
